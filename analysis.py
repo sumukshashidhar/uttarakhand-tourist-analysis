b0461d2028374bc5091ac8772726d96198336590
sumukshashidhar <sumukuuu@gmail.com>
2020-11-12 11:29:24 +0530

    finished the selection algorithm

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@
 """
 A simple python program.
 """
-#TODO: Write Proper Documentation
+# TODO: Write Proper Documentation
 
 
 # IMPORTS & REQUIREMENTS
@@ -17,50 +17,57 @@ data = "./processed_data/polarity_included.csv"
 df = pd.read_csv(data)
 
 # the df has an unnecessary column
-df = df.drop(columns = ["Unnamed: 0"])
+df = df.drop(columns=["Unnamed: 0"])
 
 # quantify positivity and negativity
-df["review_verdict"] = df.review_verdict.replace({
-        "positive":1,
-        "negative":0,
-        "neutral":0.5
-    })
+df["review_verdict"] = df.review_verdict.replace({"positive": 1, "negative": 0, "neutral": 0.5})
 
 
 # make a list of our unique spots
 residences = df.spot.unique()
 
+
 def input_picker():
     print("Please select \n1. Locations\n2. Unique Spots")
     try:
         inp = int(input())
-    except:
+    except ValueError:
         print("Please enter a number")
         exit()
+    return None
 
 
 def get_good_reviews(df):
-    """
-    I am defining a good review as something that is highly
-    polar and highly subjective
-    """
+    """Sending in a dataframe to get both positive and negative reviews back
 
+    Args:
+        df (pandas dataframe): a dataframe of all reviews
+    """
     def get_some_reviews(dfx):
-       """
-       This outputs three good reviews from the ones supplied
-       """
-
-
-
-
-def plot_review_graph_for_spot(df):
-    print("We are now plotting a review graph")
-    px.scatter(
-            data_frame=df,
-            x="review_polarity",
-            y="review_subjectivity"
-            )
+        """
+        This outputs three good reviews from the ones supplied.
+        We are trying to maximise two variables here
+        """
+        maxers = []
+        for _ in range(3):
+            maxine = 0, 0, 0, None
+            for ind in dfx.index:
+                mult = dfx["review_polarity"][ind] * dfx["review_subjectivity"][ind]
+                if maxine[0] < mult:
+                    maxine = mult, dfx["review_polarity"][ind], dfx["review_subjectivity"][ind], ind
+                elif maxine[0] == mult:
+                    maxabs = abs(maxine[1] - maxine[2])
+                    contender_abs = abs(dfx["review_polarity"][ind] - dfx["review_subjectivity"][ind])
+                    if contender_abs > maxabs:
+                        maxine = mult, dfx["review_polarity"][ind], dfx["review_subjectivity"][ind], ind
+                    else:
+                        pass
+                else:
+                    pass
+            maxers.append(maxine)
+            dfx.drop([ind])
+        return maxers
 
 if __name__ == "__main__":
-    input_picker()
+input_picker()
 
