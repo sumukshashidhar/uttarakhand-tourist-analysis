644880bd599181f86ab8f164fbe8cd461b784e9d
sumukshashidhar <sumukuuu@gmail.com>
2020-11-12 09:20:49 +0530

    added some basic code

---
+++ analysis.py
@@ -0,0 +1,27 @@
+"""
+A simple python program.
+"""
+#TODO: Write Proper Documentation
+
+
+# IMPORTS & REQUIREMENTS
+# IMPORTS
+import plotly.express as px
+import pandas as pd
+
+# DATA REQUIREMENTS
+data = "./processed_data/polarity_included.csv"
+
+# preparing a pandas dataframe
+df = pd.read_csv(data)
+
+# the df has an unnecessary column
+df = df.drop(columns = ["Unnamed: 0"])
+
+# quantify positivity and negativity
+df["review_verdict"] = df.review_verdict.replace({
+        "positive":1,
+        "negative":0,
+        "neutral":0.5
+    })
+
