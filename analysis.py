63569957bee84fa5acc0082c29a646919b0f48f7
sumukshashidhar <sumukuuu@gmail.com>
2020-11-12 12:02:40 +0530

    finished sentiment analysis algorithm

--- analysis.py
+++ analysis.py
@@ -22,6 +22,7 @@ df = df.drop(columns=["Unnamed: 0"])
 # quantify positivity and negativity
 df["review_verdict"] = df.review_verdict.replace({"positive": 1, "negative": 0, "neutral": 0.5})
 
+# make a different type of index
 
 # make a list of our unique spots
 residences = df.spot.unique()
@@ -37,6 +38,10 @@ def input_picker():
     return None
 
 
+def get_review_from_index(dfa, idx):
+    return dfa.loc[dfa.index == idx].values[0]
+
+
 def get_good_reviews(df):
     """Sending in a dataframe to get both positive and negative reviews back
 
@@ -48,33 +53,28 @@ def get_good_reviews(df):
         This outputs three good reviews from the ones supplied.
         We are trying to maximise two variables here
         """
-        maxers = []
-        for _ in range(3):
-            maxine = 0, 0, 0, None
-            for ind in dfx.index:
-                mult = dfx["review_polarity"][ind] * dfx["review_subjectivity"][ind]
-                if maxine[0] < mult:
+        maxine = 0, 0, 0, None
+        for ind in dfx.index:
+            mult = dfx["review_polarity"][ind] * dfx["review_subjectivity"][ind]
+            if maxine[0] < mult:
+                maxine = mult, dfx["review_polarity"][ind], dfx["review_subjectivity"][ind], ind
+            elif maxine[0] == mult:
+                maxabs = abs(maxine[1] - maxine[2])
+                contender_abs = abs(dfx["review_polarity"][ind] - dfx["review_subjectivity"][ind])
+                if contender_abs > maxabs:
                     maxine = mult, dfx["review_polarity"][ind], dfx["review_subjectivity"][ind], ind
-                elif maxine[0] == mult:
-                    maxabs = abs(maxine[1] - maxine[2])
-                    contender_abs = abs(dfx["review_polarity"][ind] - dfx["review_subjectivity"][ind])
-                    if contender_abs > maxabs:
-                        maxine = mult, dfx["review_polarity"][ind], dfx["review_subjectivity"][ind], ind
-                    else:
-                        pass
                 else:
                     pass
-            maxers.append(maxine[3])
-            print(maxine)
-            dfx = dfx.drop(df.index[ind])
-            print(len(dfx.index))
-        return maxers
+            else:
+                pass
+        return maxine[3]
 
-    print(len(df.index))
-    print(df.loc[df["review_verdict"] == 1])
-    pos = get_some_reviews(df.loc[df["review_verdict"] == 1])
-    # neg = get_some_reviews(df.loc[df["review_verdict"] == 0])
-    return(pos)
+    reviews = []
+    for iter in range(3):
+        val = get_some_reviews(df.loc[df["review_verdict"] == 1])
+        reviews.append(get_review_from_index(df, val)[1])
+        df = df.drop(df.index[val])
+    return(reviews)
 
 
 if __name__ == "__main__":
